ad3c5853aed3153eb2734f321acb19cd0251b807
nanakoro <dima.zhulin0@gmail.com>
2025-09-01 20:49:24 +0400

    lesson_3

--- lesson3/Task2.js
+++ lesson3/Task2.js
@@ -1,4 +1,9 @@
 const array = [2,3,4,1,2,3,4,5]
 
 // Код, который проверит, является ли массив отсортированным
-let isSortedArray = ...
+const arrayString = array.join('')
+const sortedArrayString = array.sort((a, b) => a - b ).join('')
+
+let isSortedArray = arrayString === sortedArrayString
+
+console.log(isSortedArray)
--- lesson3/Task3.js
+++ lesson3/Task3.js
@@ -1,1 +1,23 @@
+// Исходные массив
+// [1, 2, 3, 4, 10, 11, 3, 15, 20]
+// Результат работы кода
+// Четные остались на том же месте, а нечетные отсортированы
+// [1, 2, 3, 4, 10, 3, 11, 15, 20]
 
+
+function sortOddsOnly(arr) {
+    const oddsArray = arr.filter(n => n % 2 !== 0).sort((a, b) => a - b);
+
+    let oddIndex = 0;
+
+    for (let arrIndex = 0; arrIndex < arr.length; arrIndex++) {
+        if (arr[arrIndex] % 2 !== 0) {
+            arr[arrIndex] = oddsArray[oddIndex]
+            oddIndex++
+        }
+    }
+}
+
+const arr = [1, 2, 3, 4, 10, 3, 11, 15, 20]
+sortOddsOnly(arr)
+console.log(arr)
